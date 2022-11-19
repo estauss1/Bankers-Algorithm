bdfc4d26a94ed34c4c638689f5f90edc42c1c116
estauss1 <estauss23@gmail.com>
2022-11-19 18:03:10 -0500

    Initialize vectors using input file data

---
+++ bankerMain.cpp
@@ -0,0 +1,66 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+void printMatrix(std::vector<std::vector<int>> vect2D);
+
+int main(){
+
+std::ifstream matrixIFS;
+
+matrixIFS.open("matrixInput.txt");
+if(!matrixIFS.is_open()){
+    std::cerr << "Opening file failed\n";
+    exit(EXIT_FAILURE);
+}
+
+int numFromInput;
+//initialize available
+std::vector<int> available;
+for(int i = 0; i < 3; ++i){
+    matrixIFS >> numFromInput;
+    available.push_back(numFromInput);
+    //std::cout << available[i];
+}
+
+std::vector<int> temp;
+//initalize max
+std::vector<std::vector<int>> max;
+for(int i = 0; i < 5; ++i){
+    for(int i = 0; i < 3; ++i){
+        matrixIFS >> numFromInput;
+        temp.push_back(numFromInput);
+    }
+    max.push_back(temp);
+    temp.clear();
+}
+printMatrix(max);
+
+//initialize allocation
+std::vector<std::vector<int>> allocation;
+for(int i = 0; i < 5; ++i){
+    for(int i = 0; i < 3; ++i){
+        matrixIFS >> numFromInput;
+        temp.push_back(numFromInput);
+    }
+    allocation.push_back(temp);
+    temp.clear();
+}
+std::cout << std::endl;
+printMatrix(allocation);
+
+
+}
+
+
+//print vec.size x 3 matrix to console 
+void printMatrix(std::vector<std::vector<int>> vect2D){
+    std::string blank = "  ";
+    for(int i = 0; i < vect2D.size(); ++i){
+        for(int j = 0; j < 3; ++j){
+            std::cout << vect2D[i][j] << blank;
+        }
+        std::cout << std::endl;
+    }
+}
