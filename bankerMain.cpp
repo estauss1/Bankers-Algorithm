35e6efdff9e3c1fba720e3258a881c755c7e97a7
estauss1 <estauss23@gmail.com>
2022-11-20 20:29:38 -0500

    Clean up code and configure output

--- bankerMain.cpp
+++ bankerMain.cpp
@@ -19,7 +19,7 @@ if(!matrixIFS.is_open()){
     std::cerr << "Opening file failed\n";
     exit(EXIT_FAILURE);
 }
-//std::cout << "before avail initialize";
+
 int numFromInput;
 //initialize available
 std::vector<int> available;
@@ -27,7 +27,7 @@ for(int i = 0; i < 3; ++i){
     matrixIFS >> numFromInput;
     available.push_back(numFromInput);
 }
-//std::cout << "before max initialize";
+
 std::vector<int> temp;
 //initalize max
 std::vector<std::vector<int>> max;
@@ -39,8 +39,7 @@ for(int i = 0; i < 5; ++i){
     max.push_back(temp);
     temp.clear();
 }
-//printMatrix(max);
-//std::cout << "before alloc initialize";
+
 //initialize allocation
 std::vector<std::vector<int>> allocation;
 for(int i = 0; i < 5; ++i){
@@ -51,9 +50,7 @@ for(int i = 0; i < 5; ++i){
     allocation.push_back(temp);
     temp.clear();
 }
-//std::cout << std::endl;
-//printMatrix(allocation);
-//std::cout << "before need initialize";
+
 temp.resize(3);
 std::vector<std::vector<int>> need;
 for(int i = 0; i < max.size(); ++i){
@@ -61,9 +58,6 @@ for(int i = 0; i < max.size(); ++i){
         temp.begin(), [](int maxEl, int alocEl){return maxEl - alocEl;} );
     need.push_back(temp);
 }
-
-//std::cout << std::endl;
-//printMatrix(need);
 
 std::vector<int> sequence;
 int sequenceItr = 0;
@@ -75,11 +69,10 @@ std::vector<bool> finish(5, false);
 std::vector<bool> finishOverall(5, false);
 std::vector<int> work(3, 0);
 std::vector<int> availableReturned;
-//std::cout << "got to while loop";
-int tempitr = 0;
-while(sequence.size() < 5 && tempitr < 20){
-    ++tempitr;
-    std::cout << "process num is:" << processNum << std::endl;
+
+
+while(sequence.size() < 5){
+    
     //if process needs more than whats available or has already ran, go to next process
     if(!vectorIsLess(need[processNum], available) || finishOverall[processNum]){
         processNum = (processNum + 1) % 5;
@@ -90,87 +83,38 @@ while(sequence.size() < 5 && tempitr < 20){
     availableOldState = available;
     allocationOldState = allocation[processNum];
     needOldState = need[processNum];
-    /*
-    std::cout << std::endl << "need before:";
-    printVec(need[processNum]);
-    std::cout << std::endl << "avail before:";
-    printVec(available);
-    */
-
-    if(available.size() != need[processNum].size()){
-        std::cerr << need[processNum].size();
-        std::cerr << "vectors not same size a.n";
-        exit(EXIT_FAILURE);
-    }
+    
     //needed resources are taken from available
     std::transform(available.begin(), available.end(), need[processNum].begin(), 
         available.begin(), [](int availE, int needE){return availE - needE;} );
-    /*
-    std::cout << std::endl << "avail after transform:";
-    printVec(available);
-    */
 
     allocation[processNum] = max[processNum]; //process gets all needed resources 
     std::fill(need[processNum].begin(), need[processNum].end(), 0); 
-    /*
-    std::cout << std::endl << "need after fill:";
-    printVec(need[processNum]);
-    */
-    //check if safe
 
     finishOverall[processNum] = true;
     finish = finishOverall;
-
-   
-    if(available.size() != allocation[processNum].size()){
-        std::cerr << "vectors not same size a.a";
-        exit(EXIT_FAILURE);
-    }
-    /*
-    std::cout << std::endl << "allocation: ";
-    printVec(allocation[processNum]);
-    */
+    
     //allocated resources moved back to available(work)
     std::transform(available.begin(), available.end(), allocation[processNum].begin(), 
         work.begin(), [](int availE, int allocE){return availE + allocE;} );
     availableReturned = work;
     
     std::fill(allocation[processNum].begin(), allocation[processNum].end(), 0);
-
-    std::cout << std::endl << "work before loop:";
-    printVec(work);
+    
+    //check if safe
     int k = 0;
-
-    printMatrix(need);
     while(k < 5){
-        std::cout << std::endl << "NEED: ";
-        printVec(need[k]);
-        std::cout << std::endl << "AVAIL: ";
-        printVec(work);
         if(finish[k] == false && vectorIsLess(need[k], work)){
-            /*
-            if(work.size() != allocation[i].size()){
-                std::cerr << "vectors not same size w.a";
-                exit(EXIT_FAILURE);
-            */ 
-            
             std::transform(work.begin(), work.end(), allocation[k].begin(), 
                 work.begin(), [](int workE, int allocE){return workE + allocE;} );
 
             finish[k] = true;
             k = 0;
-            std::cout << std::endl <<"bool vec:";
-            printVecBool(finish);
         }else{
             ++k;
         }
     }
 
-    std::cout << std::endl << "final finish bool vec: ";
-    printVecBool(finish);
-
-
-    //if safe then all of finish should be true after for loop
     if(!allTrue(finish)){ //not safe so restore old state
         available = availableOldState;
         allocation[processNum] = allocationOldState;
@@ -178,7 +122,8 @@ while(sequence.size() < 5 && tempitr < 20){
         finishOverall[processNum] = false;
         
     }else{ //is safe
-        std::cout << "process " << processNum << " has finished";
+        std::cout << "process " << processNum << " has finished" 
+                  << std::endl;
         sequence.push_back(processNum);
         available = availableReturned;
     }
@@ -186,16 +131,16 @@ while(sequence.size() < 5 && tempitr < 20){
     std::fill(finish.begin(), finish.end(), false);
     processNum = (processNum + 1) % 5;
 
-    std::cout << "got through" << std::endl;
-
 }
 
-//print sequence
-for(int i = 0; i < sequence.size(); ++i){
-    std::cout << sequence[i] << "  ";
-
+//print safe sequence
+std::cout << std::endl << "The safe sequence is:" << std::endl;
+int w = 0;
+while(w < sequence.size() - 1){
+    std::cout << "P" << sequence[w] << " -> ";
+    ++w;
 }
-std::cout << std::endl;
+std::cout << "P" << sequence[w] << std::endl << std::endl;
 
 matrixIFS.close();
 }
@@ -203,7 +148,6 @@ matrixIFS.close();
 
 //function definitions
 
-//print vec.size x 3 matrix to console 
 void printMatrix(std::vector<std::vector<int>> vect2D){
     std::string blank = "  ";
     for(int i = 0; i < vect2D.size(); ++i){
